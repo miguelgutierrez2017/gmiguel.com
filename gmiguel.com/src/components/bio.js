0f09c6a0012ef8342eb82642d64fe75421fa488e
miguelgutierrez2017 <miguelgutierrez.a2017@gmail.com>
2025-05-08 18:43:51 -0700

    Refactor Bio component to utilize Playfair Display font and enhance link accessibility with rel attributes

--- gmiguel.com/src/components/bio.js
+++ gmiguel.com/src/components/bio.js
@@ -9,22 +9,26 @@ export default function Bio() {
       <div className="flex flex-col items-center">
         <h2 className="text-4xl font-bold mb-4">
           Hello! I&apos;m{" "}
-          <span
-            style={{
-              fontFamily: "var(--font-playfair)",
-            }}
-            className="relative inline-block text-6xl bg-gradient-to-r from-blue-600 via-purple-600 to-blue-600 bg-clip-text text-transparent after:content-[''] after:absolute after:w-full after:h-[3px] after:bottom-0 after:left-0 after:bg-gradient-to-r after:from-blue-500 after:via-purple-500 after:to-blue-500 after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-bottom-left"
-          >
+          <span className="relative inline-block text-6xl font-playfair bg-gradient-to-r from-blue-600 via-purple-600 to-blue-600 bg-clip-text text-transparent after:content-[''] after:absolute after:w-full after:h-[3px] after:bottom-0 after:left-0 after:bg-gradient-to-r after:from-blue-500 after:via-purple-500 after:to-blue-500 after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-bottom-left">
             Miguel Gutierrez
           </span>
         </h2>
         <h3 className="text-2xl font-semibold mb-2">
           IT System Analyst &nbsp;|&nbsp;
-          <Link href="/Resume.pdf" target="_blank" className="text-blue-600">
+          <Link
+            href="/Resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:text-blue-700 transition-colors"
+          >
             View Resume
           </Link>
           &nbsp;|&nbsp;
-          <Link href="/Resume.pdf" download className="text-blue-600">
+          <Link
+            href="/Resume.pdf"
+            download
+            className="text-blue-600 hover:text-blue-700 transition-colors"
+          >
             Download Resume
           </Link>
         </h3>
